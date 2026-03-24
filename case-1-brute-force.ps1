21dca5ac7f8a5c630a433ca7f2dc19ed8f071c6a
Thái <143487863+Thaiars@users.noreply.github.com>
2026-03-24 14:10:33 +0700

    Update case-1-brute-force.ps1

--- case-1-brute-force.ps1
+++ case-1-brute-force.ps1
@@ -1,7 +1,6 @@
 # Brute Force Simulation 
 auditpol /set /subcategory:"Logon" /failure:enable
 
-# Simulate 10 failed login attempts
 $targetUsers = @("FakeUser1","FakeUser2","FakeUser3","FakeUser4","FakeUser5",
                  "FakeUser6","FakeUser7","FakeUser8","FakeUser9","FakeUser10")
 
