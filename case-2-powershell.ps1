6c4bbbcac494ac9c8c99da4cc7c006b47543231e
Thái <143487863+Thaiars@users.noreply.github.com>
2026-03-24 14:19:56 +0700

    Update case-2-powershell.ps1
    Removed simulation of encoded PowerShell reconnaissance commands.

--- case-2-powershell.ps1
+++ case-2-powershell.ps1
@@ -5,7 +5,6 @@ auditpol /set /subcategory:"Process Creation" /success:enable /failure:enable
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System\Audit" `
     /v ProcessCreationIncludeCmdLine_Enabled /t REG_DWORD /d 1 /f
 
-# Simulate encoded PowerShell (reconnaissance commands)
 $command = "whoami; hostname; ipconfig"
 $encoded = [Convert]::ToBase64String([Text.Encoding]::Unicode.GetBytes($command))
 
